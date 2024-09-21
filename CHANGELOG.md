1906be6bb8d6a851ebcef7fb57914ac58337e2f6
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2024-09-21 17:00:04 +0300

    1.0.1v

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
 ## [1.0.0] - 17 Jule 2024
  - Initial Version
+## [1.0.1] - 21 September 2024
+ - Upgrarded Random Sound script and object
--- Scripts/RandomSound.cs
+++ Scripts/RandomSound.cs
@@ -3,16 +3,31 @@ using Random = UnityEngine.Random;
 
 namespace AudioSystem
 {
-    public class RandomEffectSoundOnEnable : MonoBehaviour
+    public class RandomSound : MonoBehaviour
     {
+        private enum ActivateOn
+        {
+            Enable,
+            Function,
+            Start
+        }
+
         [SerializeField] private AudioClip[] sounds;
         [SerializeField] private SoundConfiguration soundConfiguration;
+        [SerializeField] private ActivateOn activateOn;
         [SerializeField] [Range(0f, 1f)] private float volume = 1f;
         [SerializeField] private bool isFrequent = false;
 
         private void OnEnable()
         {
-            PlayRandomSound();
+            if (activateOn == ActivateOn.Enable)
+                PlayRandomSound();
+        }
+
+        private void Start()
+        {
+            if (activateOn == ActivateOn.Start)
+                PlayRandomSound();
         }
 
         private void PlayRandomSound()
